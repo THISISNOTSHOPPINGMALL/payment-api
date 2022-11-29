a4aaff7b1defb34dec027cc89d38379544699665
HanJaehee <>
2022-11-29 23:53:14 +0900

    [SHOP-25]: 모듈 세팅 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.7.6"
     id("io.spring.dependency-management") version "1.0.15.RELEASE"
-    kotlin("jvm") version "1.6.21"
-    kotlin("plugin.spring") version "1.6.21"
+    kotlin("jvm") version "1.7.0"
+    kotlin("plugin.spring") version "1.7.0"
+    kotlin("plugin.jpa") version "1.7.0"
 }
 
 group = "com.shoppingmall"
@@ -16,10 +17,38 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.projectreactor:reactor-test")
+
+    implementation("io.github.microutils:kotlin-logging:2.1.21")
+
+    // just add these dependencies for use kotlin-jdsl
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    val jdslVersion = "2.0.4.RELEASE"
+    implementation("com.linecorp.kotlin-jdsl:spring-data-kotlin-jdsl-starter:$jdslVersion")
+
+    // coroutine
+    val coroutineVersion = "1.6.3"
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:$coroutineVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactive:$coroutineVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutineVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:$coroutineVersion")
+
+    // reactive
+    implementation("com.linecorp.kotlin-jdsl:spring-data-kotlin-jdsl-hibernate-reactive:$jdslVersion")
+    implementation("org.hibernate.reactive:hibernate-reactive-core:1.1.9.Final")
+    implementation("io.smallrye.reactive:mutiny-kotlin:1.6.0")
+
+    // mysql
+    implementation("io.agroal:agroal-pool:2.0")
+    implementation("mysql:mysql-connector-java")
+    implementation("io.vertx:vertx-mysql-client:4.3.1")
 }
 
 tasks.withType<KotlinCompile> {
